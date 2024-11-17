afb1eba2528c9c0780a1474263eb647f7d67082b
JeevaKS03 <145172065+JeevaKS03@users.noreply.github.com>
2024-11-17 17:13:52 +0530

    Update login.js

--- static/login.js
+++ static/login.js
@@ -5,7 +5,7 @@ function populateEmail(){
     document.getElementById("inputmail").value=inputemail;
  }
  console.log(inputemail);
- const passowrd=document.getElementById("inputpassword");
+ const passoword=document.getElementById("inputpassword");
  const showpassword=document.getElementById("showpassword");
   showpassword.addEventListener('change',()=>{
     if(showpassword.checked){
@@ -15,11 +15,17 @@ function populateEmail(){
         passowrd.type='password';
   }
   })
-  if (inputemail=="jeevaks@kiot.ac.in" && passowrd=="jeevaks"){
-    window.location.href="/content"
-  }
 }
 window.onload=populateEmail;
+document.getElementById("button").onclick=function(){
+  const inputemail=localStorage.getItem('userEmail');
+  const password=document.getElementById("inputpassword").value;
+  console.log(inputemail);
+  console.log(password);
+  if (inputemail=="jeevaks@kiot.ac.in" && password=="jeevaks"){
+    window.location.href="/content";
+  }
+}
 
 
 
